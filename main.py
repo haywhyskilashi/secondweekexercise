cacb87cb222fa1ca745b6e6dde06877febd9119b
HP <haywhyskilashi@gmail.com>
2023-03-20 15:26:46 +0100

    chnages

--- main.py
+++ main.py
@@ -85,3 +85,94 @@ n3= int("%s%s%s" % (userInput, userInput, userInput))
 print(n1+n2+n3)
 
 
+# Write a Python program that prints the
+# calendar for a given month and year.
+# Note : Use 'calendar' module.
+
+
+from datetime import date
+
+todays_date = date.today()
+
+print("Current date: ", todays_date)
+
+print("Current year: ", todays_date.year)
+print("Current month: ", todays_date.month)
+print("Current day: ", todays_date.day)
+
+
+# Write a Python program to calculate the
+# number of days between two dates.
+# Sample dates : (2014, 7, 2), (2014, 7, 11)
+
+
+from datetime import date
+
+date1 = date(2014, 7, 2)
+date2 = date(2014, 7, 11)
+
+numberOfDays = date2 - date1
+
+print("The number of days between the given range of "
+      "date is: ", numberOfDays)
+
+
+# Write a Python program to get the volume of
+# a sphere with radius six.
+
+radius = 6.0
+pi = 3.14
+
+volume = (4.0/3.0) * pi * (radius**3)
+
+print("The volume of sphere is: ", volume)
+
+
+
+
+
+# Write a Python program to calculate the sum of three given numbers. If the
+# values are equal, return three times their sum.
+
+def my_function(x, y, z):
+    sum = x + y + z
+
+    if x == y == z:
+        sum = sum * 3
+    return sum
+print(my_function(1,2,3))
+print(my_function(3,3,3))
+
+
+
+
+
+# Write a Python program that determines whether a given number (accepted from the user) is even or odd,
+# and prints an appropriate message to the user.
+
+
+givenNumber = int(input("Enter a number: "))
+
+if givenNumber % 2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
+
+
+
+
+
+# Write a Python program to count
+# the number 4 in a given list.
+
+
+list2 = [1, 3, 4, 5, 2, 4, 5, 6, 8, 2, 4, 9, 0]
+print(list2.count(4))
+
+
+
+
+
+
+
+
